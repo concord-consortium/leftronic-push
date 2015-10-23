faa1bf2aa435f45c9f11166ec90707f043826c86
Aaron Unger <aunger@concord.org>
2015-10-23 12:05:26 -0700

    Allow specifying static data for older, shut-down portals

--- collect_and_push.rb
+++ collect_and_push.rb
@@ -16,26 +16,94 @@ metrics = {
 ###
 # pull data from portals
 ###
-portals = [
-  "admin.concord.org/portalstats/",
-  "codap.portal.concord.org/misc/stats.json",
-  "geniverse.concord.org/portal/misc/stats.json",
-  "has.portal.concord.org/misc/stats.json",
-  "inquiryspace.portal.concord.org/misc/stats.json",
-  "interactions.portal.concord.org/misc/stats.json",
-  "investigate.ritesproject.net/misc/stats.json",
-  "itsi.portal.concord.org/misc/stats.json",
-  "learn.concord.org/misc/stats.json",
-  "ngss-assessment.portal.concord.org/misc/stats.json"
-]
-portal_stats = portals.map{|portal|
-  puts "Getting stats from: #{portal}"
-  begin
-    JSON.parse(open("http://#{portal}").read)
-  rescue
-    puts "FAILED!"
-    {}
+portals = {
+  General: {
+    url: "http://admin.concord.org/portalstats/"
+  },
+  CODAP: {
+    url: "http://codap.portal.concord.org/misc/stats.json"
+  },
+  Genigames: {
+    # FIXME Need to find numbers...
+    data: {
+      active_classes: 0,
+      active_teachers: 0,
+      active_students: 0,
+      active_runnables: 0,
+      teachers: 0,
+      students: 0,
+      classes: 0
+    }
+  },
+  Geniverse: {
+    url: "http://geniverse.concord.org/portal/misc/stats.json",
+    data: {
+      active_classes: 1396,
+      active_teachers: 1101,
+      active_students: 20800,
+      active_runnables: 1396,
+      teachers: 1101,
+      students: 20800,
+      classes: 1396
+    }
+  },
+  HAS: {
+    url: "http://has.portal.concord.org/misc/stats.json"
+  },
+  InquirySpace: {
+    # AU 2015-10-23: Dan wasn't sure on numbers, but didn't think there were a significant amount... Trudi supplied a gut-level estimate.
+    url: "http://inquiryspace.portal.concord.org/misc/stats.json",
+    data: {
+      active_classes: 25,
+      active_teachers: 10,
+      active_students: 500,
+      active_runnables: 100,
+      teachers: 10,
+      students: 500,
+      classes: 25
+    }
+  },
+  Interactions: {
+    url: "http://interactions.portal.concord.org/misc/stats.json"
+  },
+  RITES: {
+    url: "http://investigate.ritesproject.net/misc/stats.json"
+  },
+  ITSI: {
+    # This is data from before ITSI was merged into the Learn portal. Teacher accounts were migrated, though,
+    # so we're leaving 'teachers' at 0 so we don't double-count. Classes and students weren't migrated, so
+    # we're still counting 'active_teachers'
+    url: "http://itsi-portal-2009.concord.org/misc/stats.json",
+    data: {
+      active_classes: 1129,
+      active_teachers: 407,
+      active_students: 18440,
+      active_runnables: 647,
+      teachers: 0,
+      students: 34898,
+      classes: 3910
+    }
+  },
+  Learn: {
+    url: "https://learn.concord.org/misc/stats.json"
+  },
+  NGSS: {
+    url: "http://ngss-assessment.portal.concord.org/misc/stats.json"
+  }
+}
+portal_stats = portals.map{|portal, config|
+  data = {}
+  if config[:data]
+    data = config[:data]
+  elsif config[:url]
+    puts "Getting stats from: #{portal} (#{config[:url]})"
+    begin
+      data = JSON.parse(open(config[:url]).read)
+    rescue => e
+      puts "FAILED!\n#{e}"
+    end
   end
+  data
 }
 
 portal_stats.each{|stat|
